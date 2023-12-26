070c168ca61e56777ab758fbd9317d14a6d39598
mithsabi <rag.sailakshmi@gmail.com>
2023-12-26 22:39:25 +0530

    new files are added

---
+++ Difference.java
@@ -0,0 +1,14 @@
+public class Difference {
+         public static void main(String[] args)  {
+	      int val=total();
+	       System.out.println(val);
+	 }
+	 
+	    public static int total()
+	    {
+	     int a = 9;
+	      int b= 5;
+	      int total =a - b;
+	     return total;
+	     }
+	 }
